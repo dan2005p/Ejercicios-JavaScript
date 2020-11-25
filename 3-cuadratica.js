14e9a09c0758f5157eb6dc18fe7e863542553d4d
Jaime Daniel Piratova Castro <dan2005p@gmail.com>
2020-11-24 22:11:57 -0500

    Cuadrática

--- 3-cuadratica.js
+++ 3-cuadratica.js
@@ -1,5 +1,5 @@
 'use strict'
-/*3. Se quiere crear un programa que resuelva la ecuación cuadrátiva tipo ax2 + bx + c. Para ello se ha estudiado el problema, que se ha planteado en pseudocódigo. 
+/*3. Se quiere crear un programa que resuelva la ecuación cuadrátiva tipo ax^2 + bx + c. Para ello se ha estudiado el problema, que se ha planteado en pseudocódigo. 
 Transformar el pseudocódigo que se muestra en código C y comprobar que el programa obtiene los resultados esperados para una serie de casos de prueba.*/
 
 //Declarar y pedir por pantalla variables
@@ -9,12 +9,17 @@ var a = prompt("Ingresa la variable A");
 var b = prompt("Ingresa la variable B");
 var c = prompt("Ingresa la variable C");
 
-if(Math.sqrt((b * b) - (4 * a * c)) > 0) {
-    res1 = (-b + Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
-    res2 = (-b - Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
-    res3 = (b * b)
+//Si b^2 - 4ac > 0  el número dentro del radical será positivo. Siempre podemos encontrar la raiz cuadrada de un positivo, por lo que evaluar la fórmula cuadrática resultará en dos soluciones reales (una sumando la raiz cuadrada positiva y la otra restando).
 
-    //document.write("Resultado 1: " + res1 );
-    //document.write("Resultado 2: " + res2 );
-    document.write("Resultado 3: " + res3)
+if((b * b) - (4 * a * c) > 0) {
+    
+    res1 = [(-b + Math.sqrt((b * b) - (4 * a * c)))] / (2 * a);
+    res2 = [(-b - Math.sqrt((b * b) - (4 * a * c)))] / (2 * a);
+    
+    document.write("Resultado 1: " + res1 +  "<br/>Resultado 2: " + res2  );
+} 
+
+
+else {
+    document.write("Resultado inexistente por que la raiz cuadrada ha dado menor que 0");
 } 
